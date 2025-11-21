98c2bae8ce90b4a905fddd38d0841fb8e14db84f
Erik Sorto <rcksorto@yahoo.com>
2025-11-21 00:30:00 -0800

    Add button to display more projects

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,13 +1,27 @@
+<script module>
+	/** @typedef {Object} Props
+	 * @property {any} children
+	 * @property {string} [href]
+	 * @property {(event: MouseEvent) => void} [onclick]
+	 */
+</script>
+
 <script>
-	let { children, href } = $props();
+	/** @type Props */
+	let { children, href, onclick } = $props();
+
+	const className =
+		'inline-block border bg-white px-4 py-2 transition-transform ease-in-out hover:-translate-1';
 </script>
 
 <div class="bg-black">
-	<a
-		class="inline-block border bg-white px-4 py-2 transition-transform ease-in-out hover:-translate-1"
-		{href}
-		target="_blank"
-	>
-		{@render children()}
-	</a>
+	{#if href}
+		<a class={className} {href} target="_blank">
+			{@render children()}
+		</a>
+	{:else}
+		<button class={className} {onclick}>
+			{@render children()}
+		</button>
+	{/if}
 </div>
--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,18 +1,30 @@
 <script>
+	import Button from './Button.svelte';
 	import Project from './Project.svelte';
 
 	/** @type import('./Project.svelte').Project[] */
 	const projects = [];
+
+	let showMore = $state(false);
 </script>
 
 {#if projects.length !== 0}
-	<div class="mb-4 text-center">
-		<h3 class="text-xl/10">Other noteworthy projects</h3>
-		<a class="font-mono text-sm" href="/archive">View the archive</a>
+	<div class="mb-8 text-center">
+		<h3 class="text-2xl/10">Other noteworthy projects</h3>
+		<a class="font-mono" href="/archive">View the archive</a>
 	</div>
-	<div class="grid grid-cols-3 gap-4">
-		{#each projects as project}
-			<Project {project} />
+	<div class="mb-8 grid grid-cols-3 gap-4">
+		{#each projects as project, i}
+			{#if i < 6 || showMore}
+				<Project {project} />
+			{/if}
 		{/each}
 	</div>
+	{#if projects.length > 6}
+		<div class="mb-8 flex justify-center">
+			<Button onclick={() => (showMore = !showMore)}>
+				{showMore ? 'Show Less' : 'Show More'}
+			</Button>
+		</div>
+	{/if}
 {/if}
